13e41f45b03112d5dd68d2d7fd074aa021e11d72
naotakeyoshida <naotake.yoshida@emc.com>
2017-01-09 13:15:23 +0900

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
 
-
 instlog="/home/vagrant/install.log"
 
 ## Update
 #sudo yum update -y
-#echo "`date +'%Y/%m/%d %H:%M:%S'` Updated " >> $instlog
-
+#echo "`date` Updated " >> $instlog
 
 ## Disable firewalld
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
-echo "`date +'%Y/%m/%d %H:%M:%S'` Stopped and disabled firewall service " >> $instlog
-
+echo "`date` Stopped and disabled firewall service " >> $instlog
 
 ## Add yum repo
 cd /etc/yum.repos.d/
 sudo wget http://download.opensuse.org/repositories/Virtualization:Appliances/CentOS_CentOS-6/Virtualization:Appliances.repo
 cd /home/vagrant
-echo "`date +'%Y/%m/%d %H:%M:%S'` yum repo added" >> $instlog
-
+echo "`date` yum repo added" >> $instlog
 
 ## Install required packages
 sudo yum install unzip numactl libaio wget openssl openldap hwinfo pciutils ethtool -y
-echo "`date +'%Y/%m/%d %H:%M:%S'` required packages inlstalled " >> $instlog
+echo "`` required packages inlstalled " >> $instlog
