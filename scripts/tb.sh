11ecc4d6f7ee219c681cc710bb495a2dd621a80a
naotakeyoshida <naotake.yoshida@emc.com>
2017-01-09 13:19:05 +0900

    Update tb.sh

--- scripts/tb.sh
+++ scripts/tb.sh
@@ -15,7 +15,7 @@ else
   ## Install Java 1.8
   cd /vagrant 
   if [ -e "/vagrant/jre-8u60-linux-x64.rpm" ]; then
-      echo "`date +'%Y/%m/%d %H:%M:%S'` jre-8u60 was founded " >> $instlog
+      echo "`date` jre-8u60 was founded " >> $instlog
   else
       echo "JRE is beeing downloaded. Please wate..."
       sudo wget --no-cookies --no-check-certificate --header \
@@ -23,14 +23,14 @@ else
         "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jre-8u60-linux-x64.rpm" -a /home/vagrant/wget.log
   fi
   sudo yum localinstall jre-8u60-linux-x64.rpm -y
-  echo "`date +'%Y/%m/%d %H:%M:%S'` Java 1.8 installed " >> $instlog
+  echo "`date` Java 1.8 installed " >> $instlog
 
   ## Install ScaleIO
   #cd /vagrant
   if [ -e "/vagrant/ScaleIO_Linux_v2.0.zip" ]; then
-      echo "`date +'%Y/%m/%d %H:%M:%S'` ScaleIO_Linux_v2.0.zip is founded " >> $instlog
+      echo "`date` ScaleIO_Linux_v2.0.zip is founded " >> $instlog
   else
-      echo "`date +'%Y/%m/%d %H:%M:%S'` ScaleIO will be downloaded " >> $instlog
+      echo "`date` ScaleIO will be downloaded " >> $instlog
       netbw=`tail -n 2 /home/vagrant/wget.log | sed -n 1p | cut -d " " -f 3 | cut -d "(" -f 2`
       dltime=`echo "1678 $netbw" | awk '{print $1 / $2}'`
       dltime2=`echo "$dltime 60" | awk '{print $1 / $2}'`
@@ -39,7 +39,7 @@ else
       echo "ScaleIO is beeing downloaded. Please wait..."
       echo "It will take about $ttdl min on your environment."
       sudo wget -nv http://downloads.emc.com/emc-com/usa/ScaleIO/ScaleIO_Linux_v2.0.zip
-      echo "`date +'%Y/%m/%d %H:%M:%S'` Download complete " >> $instlog
+      echo "`date` Download complete " >> $instlog
   fi
   sudo unzip -n /vagrant/ScaleIO_Linux_v2.0.zip -d /vagrant/scaleio/
 
@@ -49,12 +49,12 @@ else
   echo "ScaleIO Version : $version"
   gwrpm=`find /vagrant -name *gateway*.rpm`
   GATEWAY_ADMIN_PASSWORD=Password123! rpm -i $gwrpm --nodeps
-  echo "`date +'%Y/%m/%d %H:%M:%S'` ScaleIO GW-IM installed " >> $instlog
+  echo "`date` ScaleIO GW-IM installed " >> $instlog
   echo "================================================="
   echo "====== Installation for TB complete!       ======"
   echo "====== You can access ScaleIO IM from host ======" 
   echo "====== URL https://127.0.0.1:4431 (or 4432)======"
   echo "====== Login User=admin, PW=Password123!   ======"
   echo "================================================="
-  echo "`date +'%Y/%m/%d %H:%M:%S'`" > /home/vagrant/complete
+  echo "`date`" > /home/vagrant/complete
 fi
